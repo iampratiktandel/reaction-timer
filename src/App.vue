5e39e49a6c390d4c8fdc13fef48c623c191624ce
iampratiktandel <iampratiktandel@gmail.com>
2021-07-20 21:11:36 +0530

    feat: add block and results component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" :rank="rank" />
 </template>
 
 <script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 
 export default {
   name: 'App',
+  components: {
+    Block,
+    Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      rank: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.selectRank(this.score)
+      this.isPlaying = false
+      this.showResults = true
+    },
+    selectRank(score) {
+      if(score < 250) {
+        this.rank = 'Ninja Fingers'
+      } else if(score < 350) {
+        this.rank = 'Subtle Ninja'
+      } else {
+        this.rank = 'Snail Pace'
+      }
+    }
+  }
 }
 </script>
 
---
+++ src/components/Results.vue
@@ -0,0 +1,10 @@
+<template>
+  <p> Reaction Time : {{score}} ms </p>
+  <p> Rank: {{rank}} </p>
+</template>
+
+<script>
+  export default {
+    props: ['score', 'rank']
+  }
+</script>
